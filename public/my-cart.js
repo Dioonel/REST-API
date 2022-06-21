04077c8d076a0d112ef481701ac26f4bb85fa36a
Dioonel <dionelmartinezc@gmail.com>
2022-06-21 15:59:35 -0300

    Added delete item buttons on /my-cart, and empty the whole cart button too

--- public/my-cart.js
+++ public/my-cart.js
@@ -8,8 +8,9 @@ document.addEventListener('DOMContentLoaded', async () => {
     let cart = await executeGetCart(CART_API_URL)
     if(cart?.items?.length > 0){
         for(let item of cart?.items){
-            pushItems(item);                                                                            // Populate cart items
+            await pushItems(item);                                                                            // Populate cart items
         }
+        document.querySelector('.empty-cart-div').innerHTML += `<a class="btn btn-danger" onclick="emptyCart()"> Remove all items </a>`;
     } else {
         let myBody = document.querySelector('.full-body-div');
         myBody.innerHTML += `<p> Your cart is empty. </p>`;                                     // If the cart is empty, show message
@@ -28,8 +29,9 @@ async function executeGetCart(url){
     return response.json();
 }
 
-function pushItems(item){                                                                               // Populating the cart with the items
+async function pushItems(item){                                                                               // Populating the cart with the items
     let myBody = document.querySelector('.full-body-div');
+    console.log(item._id);
     myBody.innerHTML += `<div class="cart-item-div">
     <img class="cart-img" src="${item?.product?.image || ""}">
     <div class="cart-item-info-div">
@@ -37,6 +39,39 @@ function pushItems(item){                                                       
         <p class="cart-item-amount"> Amount: <strong>${item?.amount || 1}</strong> <span class="note"> - ($${item?.product?.price || "?"} each)</span> </p>
         <p class="cart-item-seller"> Seller: <strong>${item?.product?.seller?.username || "?"}</strong> </p>
     </div>
+    <button type="submit" class="removeItem" value="${item?._id}" onclick="popItem(event)"><i class="fa fa-trash"></i></button>
     <h3 class="cart-item-price"> $${parseInt(item?.product?.price) * parseInt(item?.amount) || 1} </h3>
 </div>`;
+}
+
+async function popItem(e){
+    let itemId = e.currentTarget.value;
+    
+    let response = await executePopItem(`${CART_API_URL}/${itemId}`);
+    location.reload();
+}
+
+async function executePopItem(url){
+    let response = await fetch(url, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    return response.json();
+}
+
+async function emptyCart(){
+    let response = await executeEmptyCart(`${CART_API_URL}/empty`);
+    location.reload();
+}
+
+async function executeEmptyCart(url){
+    let response = await fetch(url, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    return response.json();
 }
